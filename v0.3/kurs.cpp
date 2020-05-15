3f88107d94e87123fa7dd85685d4e89d895ba835
Rolandas55 <57255127+Rolandas55@users.noreply.github.com>
2020-05-15 10:50:29 +0300

    Add files via upload

---
+++ v0.3/kurs.cpp
@@ -0,0 +1,9 @@
+#include "kurs.h"
+#include<fstream>
+
+using namespace std;
+
+bool exists(const char *file){
+    ifstream infile(file);
+    return infile.good();
+}
---
+++ v0.3/kurs.h
@@ -0,0 +1,7 @@
+#ifndef KURS_H
+#define KURS_H
+#include<iostream>
+
+bool exists(const char*);
+
+#endif // KURS_H
